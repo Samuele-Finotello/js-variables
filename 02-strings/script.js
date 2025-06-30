367e970a92ca295ac7ac0fc2d325bc4fc36ba2f9
Samuele-Finotello <samuelefinotello.147@gmail.com>
2025-06-30 14:41:40 +0200

    [add] esercizio n. 2.2

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -1,13 +1,21 @@
 // 1. Dichiarazione di variabili
 // Crea una variabile chiamata name e assegnale il tuo nome.
 let name = "Samuele";
+
 // Stampa il valore di nome nella console.
 console.log(name);
 
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
+let number = 5;
+let text = "ciao";
+let bool = true;
+
 // Stampa tutti i valori nella console.
+console.log(number);
+console.log(text);
+console.log(bool);
 
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
